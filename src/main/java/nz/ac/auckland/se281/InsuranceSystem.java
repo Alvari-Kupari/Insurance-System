e9d96579ccaad1d98fe60a0cbae9b69a42a24911
Alvari <alvari.jk@gmail.com>
2023-03-21 13:46:18 +1300

    Created Profile Class, initialised database arraylist and checked if age is valid in the createNewProfile method

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,18 +1,39 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
-  public InsuranceSystem() {
-    // Only this constructor can be used (if you need to initialise fields).
-  }
+
+  // Initialising the database using an arraylist
+  ArrayList<Profile> dataBase = new ArrayList<>();
+
+  public InsuranceSystem() {}
 
   public void printDatabase() {
     // TODO: Complete this method.
   }
 
   public void createNewProfile(String userName, String age) {
-    // TODO: Complete this method.
+
+    // Check if the string age doesnt contain only digits (IE is not a number).
+    if (!(age.matches("[0-9]+"))) {
+
+      // if the string age was invalid, print the error message for invalid age, then return
+      MessageCli.INVALID_AGE.printMessage(age, userName);
+      return;
+    }
+
+    // Next we must ensure the username is valid
+
+    // Next, the age must be converted into an integer
+    int age_integer = Integer.parseInt(age);
+
+    // create the new profile
+    Profile newProfile = new Profile(userName, age_integer);
+
+    // add the new profile into the arraylist to store it
+    dataBase.add(newProfile);
   }
 
   public void loadProfile(String userName) {
---
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland.se281;
+
+public class Profile {
+  private String name;
+  private int age;
+
+  public Profile(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+}
