8caadf626b70e57338d424fb7619d169cd3783e0
Alvari <alvari.jk@gmail.com>
2023-03-21 16:23:55 +1300

    completed createNewProfile method with few small bugs

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -16,24 +16,54 @@ public class InsuranceSystem {
 
   public void createNewProfile(String userName, String age) {
 
+    // Format the username to title case by first converting the whole string to lowercase
+    userName.toLowerCase();
+
+    // then change the first letter to uppercase
+    String userNameFormatted = userName.substring(0, 1).toUpperCase() + userName.substring(1);
+
     // Check if the string age doesnt contain only digits (IE is not a number).
     if (!(age.matches("[0-9]+"))) {
 
       // if the string age was invalid, print the error message for invalid age, then return
-      MessageCli.INVALID_AGE.printMessage(age, userName);
+      MessageCli.INVALID_AGE.printMessage(age, userNameFormatted);
       return;
     }
-
-    // Next we must ensure the username is valid
 
     // Next, the age must be converted into an integer
     int age_integer = Integer.parseInt(age);
 
+    // Next we must ensure the username is valid (more than 2 characters)
+    if (userNameIsTooSHort(userName)) {
+      // Print error message for username too short
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userNameFormatted);
+      return;
+    }
+
+    // Next ensure the username is unique
+    int a = 0;
+    for (int i = 0; i < dataBase.size(); i++) {
+      Profile temporaryProfile = dataBase.get(i);
+
+      if (temporaryProfile.name == userNameFormatted) {
+        // if it wasnt unique then print the correct error message and exit the method
+        MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userNameFormatted);
+        a = 1;
+        return;
+      }
+    }
+
     // create the new profile
-    Profile newProfile = new Profile(userName, age_integer);
+    Profile newProfile = new Profile(userNameFormatted, age_integer);
 
     // add the new profile into the arraylist to store it
     dataBase.add(newProfile);
+
+    // Print the message showing the new profile was successfully created
+    MessageCli.PROFILE_CREATED.printMessage(userNameFormatted, age);
+    if (a == 1) {
+      System.out.println("doggies");
+    }
   }
 
   public void loadProfile(String userName) {
@@ -50,5 +80,17 @@ public class InsuranceSystem {
 
   public void createPolicy(PolicyType type, String[] options) {
     // TODO: Complete this method.
+  }
+
+  // Helper methods
+  public boolean userNameIsTooSHort(String userName) {
+
+    // Check the length (if less than 3 then return true)
+    if (userName.length() < 3) {
+      return true;
+    }
+
+    // else return false
+    return false;
   }
 }
--- src/main/java/nz/ac/auckland/se281/Profile.java
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -1,8 +1,8 @@
 package nz.ac.auckland.se281;
 
 public class Profile {
-  private String name;
-  private int age;
+  public String name;
+  public int age;
 
   public Profile(String name, int age) {
     this.name = name;
